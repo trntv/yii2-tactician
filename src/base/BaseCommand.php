2529f9f94ae6b124cb6ed31f126f1a0444668297
deanar <rdeanar@gmail.com>
2018-01-08 16:00:34 +0300

    Use `yii\base\BaseObject` instead of `yii\base\Object`. Require yii2 minimum version 2.0.13

--- src/base/BaseCommand.php
+++ src/base/BaseCommand.php
@@ -2,12 +2,12 @@
 
 namespace trntv\tactician\base;
 
-use yii\base\Object;
+use yii\base\BaseObject;
 
 /**
  * @author Eugene Terentev <eugene@terentev.net>
  */
-class BaseCommand extends Object
+class BaseCommand extends BaseObject
 {
 
 }
--- src/base/BaseHandler.php
+++ src/base/BaseHandler.php
@@ -2,12 +2,12 @@
 
 namespace trntv\tactician\base;
 
-use yii\base\Object;
+use yii\base\BaseObject;
 
 /**
  * @author Eugene Terentev <eugene@terentev.net>
  */
-abstract class BaseHandler extends Object
+abstract class BaseHandler extends BaseObject
 {
     /**
      * @param BaseCommand $command
